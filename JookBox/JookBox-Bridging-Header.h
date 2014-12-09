7a5420d6360684aa86a6c1b91cefb4d615860e7d
Andrew Barba <abarba.77@gmail.com>
2014-12-08 21:57:48 -0500

    setup, add spotify sdk

---
+++ JookBox/JookBox-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  JookBox-Bridging-Header.h
+//  JookBox
+//
+//  Created by Andrew Barba on 12/8/14.
+//  Copyright (c) 2014 Andrew Barba. All rights reserved.
+//
+
+#ifndef JookBox_JookBox_Bridging_Header_h
+#define JookBox_JookBox_Bridging_Header_h
+
+#import <Spotify/Spotify.h>
+
+#endif
--- Podfile
+++ Podfile
@@ -1,7 +1,7 @@
 platform :ios, '8.0'
 
 target 'JookBox' do
-
+    pod 'SDWebImage'
 end
 
 target 'JookBoxTests' do
